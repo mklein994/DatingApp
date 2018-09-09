f23b4486d0cd21090ef6c4bd666738f41cfd64ea
Matthew Klein <mklein994@gmail.com>
2018-09-09 02:28:21 -0500

    Allow CORS for any origin, method or header

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,6 +28,7 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -44,6 +45,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
