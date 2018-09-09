7cf943187284f181caae63f03bcb66e2b05e44b0
Matthew Klein <mklein994@gmail.com>
2018-09-09 01:55:43 -0500

    Show values from the API in the component

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ValueComponent } from './value/value.component';
+
+const routes: Routes = [
+  { path: 'values', component: ValueComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -1,21 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface Value {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
   styleUrls: ['./value.component.scss'],
 })
 export class ValueComponent implements OnInit {
-  value: any;
+  values: Value[];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getValues();
+  }
 
   getValues() {
     this.http
-      .get('http://localhost:5000/api/values')
-      .subscribe(values => (this.value = values));
+      .get<Value[]>('http://localhost:5000/api/values')
+      .subscribe(values => (this.values = values), error => console.log(error));
   }
 }
