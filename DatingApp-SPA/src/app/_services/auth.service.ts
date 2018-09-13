077aecef5597f845096655ba5449f3c055f11f84
Matthew Klein <mklein994@gmail.com>
2018-09-12 21:40:26 -0500

    Add trailing slash to API url

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -6,12 +6,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth';
+  baseUrl = 'http://localhost:5000/api/auth/';
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
-    return this.http.post(`${this.baseUrl}/login`, model).pipe(
+    return this.http.post(`${this.baseUrl}login`, model).pipe(
       map((response: any) => {
         const user = response;
         if (user) {
