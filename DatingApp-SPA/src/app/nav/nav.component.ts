ad420d9021eb14516ff75ec96b990f682538c04e
Matthew Klein <mklein994@gmail.com>
2018-09-14 15:42:12 -0500

    Use alertify in nav and register components

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
@@ -10,7 +11,10 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+  ) {}
 
   ngOnInit() {}
 
@@ -18,8 +22,8 @@ export class NavComponent implements OnInit {
     this.authService
       .login(this.model)
       .subscribe(
-        next => console.log('Logged in successfully'),
-        error => console.log(error),
+        next => this.alertify.success('Logged in successfully'),
+        error => this.alertify.error(error),
       );
   }
 
@@ -30,6 +34,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
