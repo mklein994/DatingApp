b6f132ad31ecde6e170560bdc646fb51055ada3d
Matthew Klein <mklein994@gmail.com>
2018-09-16 18:03:34 -0500

    Implement the member-card component side panel

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../_models/user';
+import { AlertifyService } from '../../_services/alertify.service';
+import { UserService } from '../../_services/user.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.scss'],
 })
 export class MemberDetailComponent implements OnInit {
-  constructor() {}
+  user: User;
 
-  ngOnInit() {}
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  // GET members/3
+  loadUser() {
+    this.userService
+      .getUser(+this.route.snapshot.params['id'])
+      .subscribe(
+        (user: User) => (this.user = user),
+        error => this.alertify.error(error),
+      );
+  }
 }
