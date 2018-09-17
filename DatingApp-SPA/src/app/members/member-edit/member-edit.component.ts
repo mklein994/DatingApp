10bbe3e2c4cbda1b8a75e4ce5b4ff93a4e6b313b
Matthew Klein <mklein994@gmail.com>
2018-09-17 16:47:20 -0500

    Confirm before leaving unsaved changes to profile

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
@@ -14,6 +14,12 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm')
   editForm: NgForm;
   user: User;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private route: ActivatedRoute,
