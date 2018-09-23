c19ce89dfdd07be785845025888d7e76c316cfab
Matthew Klein <mklein994@gmail.com>
2018-09-23 16:59:35 -0500

    Setup API method to set main photo

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,11 @@ namespace DatingApp.API.Data
         public void Add<T>(T entity) where T : class => _context.Add(entity);
 
         public void Delete<T>(T entity) where T : class => _context.Remove(entity);
+
+        public async Task<Photo> GetMainPhotoForUser(int userId) =>
+            await _context.Photos
+                .Where(u => u.UserId == userId)
+                .FirstOrDefaultAsync(p => p.IsMain);
 
         public async Task<Photo> GetPhoto(int id) => await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
         public async Task<User> GetUser(int id) => await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
