464d869b79b510bb1ac477b826b63e458c80174b
Matthew Klein <mklein994@gmail.com>
2018-09-24 11:47:05 -0500

    Return a paged list of users asking for page info
    Example: if there are 10 users with ids from 1 to 10, querying
    /api/users?pageNumber=2&pageSize=3 would return page two of four
    containing users 4, 5 and 6.
    
    Defaults to page size 10, page number 1, and caps the page size at 50.

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,7 +27,12 @@ namespace DatingApp.API.Data
         public async Task<Photo> GetPhoto(int id) => await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
         public async Task<User> GetUser(int id) => await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
 
-        public async Task<IEnumerable<User>> GetUsers() => await _context.Users.Include(p => p.Photos).ToListAsync();
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
+        {
+            var users = _context.Users.Include(p => p.Photos);
+
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
+        }
 
         public async Task<bool> SaveAll() => await _context.SaveChangesAsync() > 0;
     }
