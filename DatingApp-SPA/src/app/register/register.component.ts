0920f6c7b91cfe09920137a98b60b6e46a4c8cea
Matthew Klein <mklein994@gmail.com>
2018-09-24 10:25:16 -0500

    Re-enable register method with the register form
    This form was built using a `FormBuilder`. Navigate to '/members' once
    they successfully register.

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -5,8 +5,10 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
 
+import { User } from '../_models/user';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -18,7 +20,7 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Output()
   cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
@@ -26,6 +28,7 @@ export class RegisterComponent implements OnInit {
     private authService: AuthService,
     private alertify: AlertifyService,
     private fb: FormBuilder,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -65,13 +68,19 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService
-    //   .register(this.model)
-    //   .subscribe(
-    //     () => this.alertify.success('registration successful'),
-    //     error => this.alertify.error(error),
-    //   );
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService
+        .register(this.user)
+        .subscribe(
+          () => this.alertify.success('Registration successful'),
+          error => this.alertify.error(error),
+          () =>
+            this.authService
+              .login(this.user)
+              .subscribe(() => this.router.navigate(['/members'])),
+        );
+    }
   }
 
   cancel() {
