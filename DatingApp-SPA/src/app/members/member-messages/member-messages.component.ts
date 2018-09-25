9f6d514931f154049a41a36cae797d29bb539444
Matthew Klein <mklein994@gmail.com>
2018-09-25 12:38:14 -0500

    Implement a form to send a message

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -14,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input()
   recipientId: number;
   messages: Message[];
+  newMessage: any = {};
 
   constructor(
     private userService: UserService,
@@ -30,6 +31,19 @@ export class MemberMessagesComponent implements OnInit {
       .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
       .subscribe(
         messages => (this.messages = messages),
+        error => this.alertify.error(error),
+      );
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService
+      .sendMessage(this.authService.decodedToken.nameid, this.newMessage)
+      .subscribe(
+        (message: Message) => {
+          this.messages.unshift(message);
+          this.newMessage.content = '';
+        },
         error => this.alertify.error(error),
       );
   }
