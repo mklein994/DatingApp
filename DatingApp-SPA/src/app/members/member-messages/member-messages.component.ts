f657dd3c3e1a1823b0f761144bd3a3ed1b2d95ea
Matthew Klein <mklein994@gmail.com>
2018-09-25 09:56:43 -0500

    Show messages on user's messages tab on their profile

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Message } from '../../_models/message';
+import { AlertifyService } from '../../_services/alertify.service';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
 
 @Component({
   selector: 'app-member-messages',
@@ -6,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-messages.component.scss'],
 })
 export class MemberMessagesComponent implements OnInit {
-  constructor() {}
+  @Input()
+  recipientId: number;
+  messages: Message[];
 
-  ngOnInit() {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private alertify: AlertifyService,
+  ) {}
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.userService
+      .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .subscribe(
+        messages => (this.messages = messages),
+        error => this.alertify.error(error),
+      );
+  }
 }
