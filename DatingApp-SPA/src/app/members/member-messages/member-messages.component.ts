7df17a24f3c8c555def7250fed9742f3249043ce
Matthew Klein <mklein994@gmail.com>
2018-09-25 14:39:09 -0500

    Mark messages as read once viewed by the recipient

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 import { Message } from '../../_models/message';
 import { AlertifyService } from '../../_services/alertify.service';
@@ -27,8 +28,21 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService
       .getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (
+              messages[i].isRead === false &&
+              messages[i].recipientId === currentUserId
+            ) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        }),
+      )
       .subscribe(
         messages => (this.messages = messages),
         error => this.alertify.error(error),
