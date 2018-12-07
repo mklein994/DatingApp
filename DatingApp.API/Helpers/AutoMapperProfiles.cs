a535032128529fb72438cbb652a7fad7eda200d7
Matthew Klein <mklein994@gmail.com>
2018-12-07 16:22:18 -0600

    Upgrade AutoMapper to 6.0.0 and fix breaking changes
    `ResolveUsing()` has been merged with `MapFrom()`, so use that instead.

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -12,11 +12,11 @@ namespace DatingApp.API.Helpers
         {
             CreateMap<User, UserForListDto>()
             .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain).Url))
-            .ForMember(d => d.Age, o => o.ResolveUsing(s => s.DateOfBirth.CalculateAge()));
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()));
 
             CreateMap<User, UserForDetailedDto>()
             .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(p => p.IsMain).Url))
-            .ForMember(d => d.Age, o => o.ResolveUsing(s => s.DateOfBirth.CalculateAge()));
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotosForDetailedDto>();
             CreateMap<Photo, PhotoForReturnDto>();
